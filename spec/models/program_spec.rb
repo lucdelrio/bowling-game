44c6c3586f502164a9991e562a230b9efc08bf18
Lucas del Rio <lucasgdelrio93@gmail.com>
2020-02-17 20:32:11 -0300

    Adds integration test for program class

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -8,4 +8,14 @@ describe 'Program' do
       expect { Program.new(nil) }.to raise_error(NoFileException)
     end
   end
+
+  context 'with arguments' do
+    it 'execute fine' do
+      file_param = File.join(File.dirname(__FILE__), './../fixtures/one_player_rolls.txt')
+      result = "Frame     1       2       3       4       5       6       7       8       9       10      \nCarl
+Pinfalls       X       X       X       X       X       X       X       X       X   X   X   X
+Score     30      60      90      120     150     180     210     240     270     300     \n"
+      expect { Program.new(file_param) }.to output(result).to_stdout
+    end
+  end
 end
